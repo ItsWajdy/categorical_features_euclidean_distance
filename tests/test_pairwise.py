c5bc65ca201c883e9dda3bc621d935bad06984ba
Wajdy <itswajdy@gmail.com>
2020-08-08 14:24:32 +0300

    Add tests for split Euclidean distances
    - Add tests for empty DataFrames checking
    - Add tests for DataFrames with different lengths
    - Add tests for DataFrames with different number of columns
    - Add tests for DataFrames with different column names
    - Add tests for DataFrames with different column data types

--- tests/test_pairwise.py
+++ tests/test_pairwise.py
@@ -1,4 +1,5 @@
 from cfed.pairwise import euclidean_distances
+from cfed.pairwise import euclidean_distances_from_split
 import pandas as pd
 import numpy as np
 import sklearn.metrics.pairwise as pw
@@ -18,6 +19,31 @@ def test_basic_functionality():
 
     distances = euclidean_distances(df1, df2, categorical_columns=['col1'])
     assert distances.shape == (df1.shape[0], df2.shape[0])
+    assert not any(elem == np.nan for row in distances for elem in row)
+
+
+def test_split_basic_functionality():
+    df1_numerical = pd.DataFrame.from_dict({
+        'col1': [1, 2, 3],
+        'col2': [4, 5, 6],
+        'col3': [7, 8, 9]
+    })
+    df2_numerical = pd.DataFrame.from_dict({
+        'col1': [1, 4, 7],
+        'col2': [2, 5, 8],
+        'col3': [3, 6, 9],
+    })
+
+    df1_categorical = pd.DataFrame.from_dict({
+        'col4': [1, 1, 2],
+    })
+
+    df2_categorical = pd.DataFrame.from_dict({
+        'col4': [1, 2, 2],
+    })
+
+    distances = euclidean_distances_from_split(df1_numerical, df1_categorical, df2_numerical, df2_categorical)
+    assert distances.shape == (df1_numerical.shape[0], df2_numerical.shape[0])
     assert not any(elem == np.nan for row in distances for elem in row)
 
 
@@ -85,6 +111,240 @@ def test_dataframes_validation():
     __test_different_column_dtypes()
 
 
+def __test_split_empty_dataframes():
+    try:
+        df1_numerical = pd.DataFrame()
+        df2_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 4, 7],
+            'col2': [2, 5, 8],
+            'col3': [3, 6, 9],
+        })
+
+        df1_categorical = pd.DataFrame()
+        df2_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 2, 2],
+            'col5': [5, 6, 5],
+        })
+
+        euclidean_distances_from_split(df1_numerical, df1_categorical, df2_numerical, df2_categorical)
+        assert False, 'DataFrame validation failed'
+    except AssertionError as e:
+        pass
+
+    try:
+        df1_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 2, 3],
+            'col2': [4, 5, 6],
+            'col3': [7, 8, 9],
+            'col5': [9, 1, 6],
+        })
+        df2_numerical = pd.DataFrame()
+
+        df1_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 1, 2],
+        })
+        df2_categorical = pd.DataFrame()
+
+        euclidean_distances_from_split(df1_numerical, df1_categorical, df2_numerical, df2_categorical)
+        assert False, 'DataFrame validation failed'
+    except AssertionError as e:
+        pass
+
+
+def __test_split_different_length():
+    try:
+        df1_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 2, 3],
+            'col2': [4, 5, 6],
+            'col3': [7, 8, 9],
+        })
+        df2_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 4, 7, 8],
+            'col2': [2, 5, 8, 9],
+            'col3': [3, 6, 9, 0],
+        })
+
+        df1_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 1, 2],
+        })
+
+        df2_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 2, 2, 3],
+        })
+
+        euclidean_distances_from_split(df1_numerical, df1_categorical, df2_numerical, df2_categorical)
+        assert False, 'DataFrame validation failed'
+    except AssertionError as e:
+        pass
+
+
+def __test_split_different_column_count():
+    try:
+        df1_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 2, 3],
+            'col2': [4, 5, 6],
+            'col3': [7, 8, 9],
+        })
+        df2_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 4, 7],
+            'col2': [2, 5, 8],
+            'col3': [3, 6, 9],
+        })
+
+        df1_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 1, 2],
+        })
+
+        df2_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 2, 2],
+            'col5': [5, 6, 5],
+        })
+
+        euclidean_distances_from_split(df1_numerical, df1_categorical, df2_numerical, df2_categorical)
+        assert False, 'DataFrame validation failed'
+    except AssertionError as e:
+        pass
+
+    try:
+        df1_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 2, 3],
+            'col2': [4, 5, 6],
+            'col3': [7, 8, 9],
+            'col5': [9, 1, 6],
+        })
+        df2_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 4, 7],
+            'col2': [2, 5, 8],
+            'col3': [3, 6, 9],
+        })
+
+        df1_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 1, 2],
+        })
+
+        df2_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 2, 2],
+        })
+
+        euclidean_distances_from_split(df1_numerical, df1_categorical, df2_numerical, df2_categorical)
+        assert False, 'DataFrame validation failed'
+    except AssertionError as e:
+        pass
+
+
+def __test_split_different_column_names():
+    try:
+        df1_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 2, 3],
+            'col2': [4, 5, 6],
+            'col4': [7, 8, 9],
+        })
+        df2_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 4, 7],
+            'col2': [2, 5, 8],
+            'col3': [3, 6, 9],
+        })
+
+        df1_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 1, 2],
+        })
+
+        df2_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 2, 2],
+        })
+
+        euclidean_distances_from_split(df1_numerical, df1_categorical, df2_numerical, df2_categorical)
+        assert False, 'DataFrame validation failed'
+    except AssertionError as e:
+        pass
+
+    try:
+        df1_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 2, 3],
+            'col2': [4, 5, 6],
+            'col3': [7, 8, 9],
+        })
+        df2_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 4, 7],
+            'col2': [2, 5, 8],
+            'col3': [3, 6, 9],
+        })
+
+        df1_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 1, 2],
+        })
+
+        df2_categorical = pd.DataFrame.from_dict({
+            'col5': [1, 2, 2],
+        })
+
+        euclidean_distances_from_split(df1_numerical, df1_categorical, df2_numerical, df2_categorical)
+        assert False, 'DataFrame validation failed'
+    except AssertionError as e:
+        pass
+
+
+def __test_split_different_column_dtypes():
+    try:
+        df1_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 2, 3],
+            'col2': [4, 5, 6],
+            'col3': [7, 8, '9'],
+        })
+
+        df2_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 4, 7],
+            'col2': [2, 5, 8],
+            'col3': [3, 6, 9],
+        })
+
+        df1_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 1, 2],
+        })
+
+        df2_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 2, 2],
+        })
+
+        euclidean_distances_from_split(df1_numerical, df1_categorical, df2_numerical, df2_categorical)
+        assert False, 'DataFrame validation failed'
+    except AssertionError as e:
+        pass
+
+    try:
+        df1_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 2, 3],
+            'col2': [4, 5, 6],
+            'col3': [7, 8, 9],
+        })
+        df2_numerical = pd.DataFrame.from_dict({
+            'col1': [1, 4, 7],
+            'col2': [2, 5, 8],
+            'col3': [3, 6, 9],
+        })
+
+        df1_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 1, 2],
+        })
+
+        df2_categorical = pd.DataFrame.from_dict({
+            'col4': [1, 2, '2'],
+        })
+
+        euclidean_distances_from_split(df1_numerical, df1_categorical, df2_numerical, df2_categorical)
+        assert False, 'DataFrame validation failed'
+    except AssertionError as e:
+        pass
+
+
+def test_split_dataframes_validation():
+    __test_split_empty_dataframes()
+    __test_split_different_length()
+    __test_split_different_column_count()
+    __test_split_different_column_names()
+    __test_split_different_column_dtypes()
+
+
 def test_equality_no_categorical_features():
     df1 = pd.DataFrame.from_dict({
         'col1': [1, 2, 3],
@@ -120,3 +380,6 @@ def test_non_equality_with_categorical_features():
     ground_truth = pw.euclidean_distances(df1.values, df2.values)
 
     assert not np.array_equal(predicted_truth, ground_truth)
+
+
+test_split_dataframes_validation()
